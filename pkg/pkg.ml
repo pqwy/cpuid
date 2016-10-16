216a0550177b7198e9d653760907996dd36a5101
pqwy <david@numm.org>
2016-10-16 04:20:19 +0100

    stubblr

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,10 +1,12 @@
 #!/usr/bin/env ocaml
 #use "topfind"
 #require "topkg"
+#require "ocb-stubblr.topkg"
 open Topkg
 
+let build = Pkg.build ~cmd:Ocb_stubblr_topkg.cmd ()
 let () =
-  Pkg.describe "cpuid" @@ fun c ->
+  Pkg.describe ~build "cpuid" @@ fun c ->
   Ok [ Pkg.mllib "src/cpuid.mllib";
        Pkg.clib "src/libcpuid_stubs.clib";
        Pkg.test "test/test"; ]
