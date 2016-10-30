aed39222bebdd7dd13e9d21ffcd7dfe6f6a4d469
pqwy <david@numm.org>
2016-10-30 04:45:22 +0000

    track ocb-stubblr

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,6 +1,2 @@
 open Ocamlbuild_plugin
-
-(* let () = dispatch Ocb_stubblr.init *)
-
-(* <= 4.02.X *)
-let () = dispatch Ocb_stubblr.(init & ccopt_flags "--std=c99 -Wall -Wextra -O3")
+let () = dispatch Ocb_stubblr.init
